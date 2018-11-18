35ecc33485020cd1c69e2d9b7a5e5c9aa2a9c672
Elena Pavlova <lenlho22@gmail.com>
2018-11-18 20:00:08 +0300

    Created CatalogNavigation by local state

--- src/components/CatalogNavigation/CatalogNavigation.js
+++ src/components/CatalogNavigation/CatalogNavigation.js
@@ -2,7 +2,20 @@ import React from 'react'
 import CatalogNavigationItem from '../CatalogNavigationItem'
 
 class CatalogNavigation extends React.Component {
+  state = [
+    { id: '1', name: 'Электроника' },
+    { id: '2', name: 'Компьютеры и сети' },
+    { id: '3', name: 'Бытовая техника' },
+    { id: '4', name: 'Стройка и ремонт' },
+    { id: '5', name: 'Дом и сад' },
+    { id: '6', name: 'Авто и мото' },
+    { id: '7', name: 'Красота и спорт' },
+    { id: '8', name: 'Детям и мамам' },
+    { id: '9', name: 'Работа и офис' },
+  ]
   render() {
+    const items = this.state
+    console.log('items', items)
     return (
       <React.Fragment>
         <link rel="stylesheet" href="./Товары в каталоге Onliner.by_files/catalog-navigation.css" />
@@ -21,62 +34,9 @@ class CatalogNavigation extends React.Component {
                 display: 'none',
               }}
             />
-            <CatalogNavigationItem />
-            <CatalogNavigationItem />
-            <li className="catalog-navigation-classifier__item " data-id={3}>
-              <span className="catalog-navigation-classifier__item-icon catalog-navigation-classifier__item-icon_id_3" />
-              <span className="catalog-navigation-classifier__item-title">
-                <span className="catalog-navigation-classifier__item-title-wrapper">
-                  Бытовая техника
-                </span>
-              </span>
-            </li>
-            <li className="catalog-navigation-classifier__item " data-id={4}>
-              <span className="catalog-navigation-classifier__item-icon catalog-navigation-classifier__item-icon_id_4" />
-              <span className="catalog-navigation-classifier__item-title">
-                <span className="catalog-navigation-classifier__item-title-wrapper">
-                  Стройка и ремонт
-                </span>
-              </span>
-            </li>
-            <li className="catalog-navigation-classifier__item " data-id={5}>
-              <span className="catalog-navigation-classifier__item-icon catalog-navigation-classifier__item-icon_id_5" />
-              <span className="catalog-navigation-classifier__item-title">
-                <span className="catalog-navigation-classifier__item-title-wrapper">Дом и сад</span>
-              </span>
-            </li>
-            <li className="catalog-navigation-classifier__item " data-id={6}>
-              <span className="catalog-navigation-classifier__item-icon catalog-navigation-classifier__item-icon_id_6" />
-              <span className="catalog-navigation-classifier__item-title">
-                <span className="catalog-navigation-classifier__item-title-wrapper">
-                  Авто и мото
-                </span>
-              </span>
-            </li>
-            <li className="catalog-navigation-classifier__item " data-id={7}>
-              <span className="catalog-navigation-classifier__item-icon catalog-navigation-classifier__item-icon_id_7" />
-              <span className="catalog-navigation-classifier__item-title">
-                <span className="catalog-navigation-classifier__item-title-wrapper">
-                  Красота и спорт
-                </span>
-              </span>
-            </li>
-            <li className="catalog-navigation-classifier__item " data-id={8}>
-              <span className="catalog-navigation-classifier__item-icon catalog-navigation-classifier__item-icon_id_8" />
-              <span className="catalog-navigation-classifier__item-title">
-                <span className="catalog-navigation-classifier__item-title-wrapper">
-                  Детям и мамам
-                </span>
-              </span>
-            </li>
-            <li className="catalog-navigation-classifier__item " data-id={9}>
-              <span className="catalog-navigation-classifier__item-icon catalog-navigation-classifier__item-icon_id_9" />
-              <span className="catalog-navigation-classifier__item-title">
-                <span className="catalog-navigation-classifier__item-title-wrapper">
-                  Работа и офис
-                </span>
-              </span>
-            </li>
+            {items.map(({ id, name }) => (
+              <CatalogNavigationItem key={id} name={name} />
+            ))}
           </ul>
         </div>
       </React.Fragment>
--- src/components/CatalogNavigationItem/CatalogNavigationItem.js
+++ src/components/CatalogNavigationItem/CatalogNavigationItem.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 class CatalogNavigationItem extends React.Component {
   render() {
+    const { id, name } = this.props
+    console.log(`!!! this.props ${JSON.stringify(this.props)}`)
     return (
-      <li className="catalog-navigation-classifier__item " data-id={2}>
+      <li className="catalog-navigation-classifier__item " key={id}>
         <span className="catalog-navigation-classifier__item-icon catalog-navigation-classifier__item-icon_id_2" />
         <span className="catalog-navigation-classifier__item-title">
-          <span className="catalog-navigation-classifier__item-title-wrapper">
-            Компьютеры и сети
-          </span>
+          <span className="catalog-navigation-classifier__item-title-wrapper">{name}</span>
         </span>
       </li>
     )
