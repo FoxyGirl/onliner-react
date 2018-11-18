cb0b194a5d1bf3f9fdb0cb7c3ad02f3135b2603e
Elena Pavlova <lenlho22@gmail.com>
2018-11-18 12:35:21 +0300

    Created base App skeleton

--- src/App.js
+++ src/App.js
@@ -7,29 +7,59 @@ import './App.css'
 
 class App extends Component {
   render() {
-    const {
-      route: { routes },
-    } = this.props
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/basket">Basket</Link>
-          {renderRoutes(routes)}
-          <Button type="primary"> And Design</Button>
-        </header>
+      <div class="layout-container">
+        <div dangerouslySetInnerHTML={{ __html: '<!-- Container -->' }} />
+        <div id="container">
+          <div class="g-container-outer">
+            <div class="bnr-top-wide bnr-top-wide_fixed-height">
+              <script async="async" src="./Товары в каталоге Onliner.by_files/gpt.js" />
+
+              <div
+                dangerouslySetInnerHTML={{ __html: '<!-- /312496551/catalog_970x90(ATF) -->' }}
+              />
+
+              <div id="div-gpt-ad-1476699139023-0">
+                <div
+                  id="google_ads_iframe_/312496551/catalog_970x90(ATF)_0__container__"
+                  style={{ border: '0pt none' }}
+                >
+                  <iframe
+                    id="google_ads_iframe_/312496551/catalog_970x90(ATF)_0"
+                    title="3rd party ad content"
+                    name="google_ads_iframe_/312496551/catalog_970x90(ATF)_0"
+                    width="728"
+                    height="90"
+                    scrolling="no"
+                    marginwidth="0"
+                    marginheight="0"
+                    frameborder="0"
+                    style={{ border: '0px', verticalAlign: 'bottom' }}
+                    src="./Товары в каталоге Onliner.by_files/saved_resource.html"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="l-gradient-wrapper">
+              <div dangerouslySetInnerHTML={{ __html: '<!-- Top -->' }} />
+
+              <div dangerouslySetInnerHTML={{ __html: '<!-- Top -->' }} />
+
+              <noscript>You need to enable JavaScript to run this app.</noscript>
+
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>Здесь будет React для onliner.</p>
+                </header>
+              </div>
+
+              <div dangerouslySetInnerHTML={{ __html: '<!-- Bottom -->' }} />
+
+              <div dangerouslySetInnerHTML={{ __html: '<!-- Bottom -->' }} />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
